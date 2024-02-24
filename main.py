3f95724a15c59ceb9c33d911407fe81da727ef45
lilfinniiii <the.finn.parnishka@gmail.com>
2024-02-24 17:49:45 +0300

    new list - upd

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from config import TOKEN_API
 
+
 async def user_db(user_id):
     db_name = f"user_{user_id}_todos.db"
     conn = sqlite3.connect(db_name)
@@ -16,15 +17,12 @@ async def user_db(user_id):
         CREATE TABLE IF NOT EXISTS tasks (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
+            description TEXT,
             status TEXT
         )
     ''')
     conn.commit()
     return conn, cursor
-
-
-
-
 
 
 logging.basicConfig(level=logging.INFO)
@@ -34,20 +32,25 @@ dp = Dispatcher()
 valid_statuses = ['todo', 'doing', 'done']
 
 
-
 class AddTaskStates(StatesGroup):
     task = State()
+    description = State()
     status = State()
 
+
 class DeleteTask(StatesGroup):
-    waiting_for_status = State()
     waiting_for_delete = State()
 
+
 class MoveTask(StatesGroup):
-    move_status = State()
     move_number = State()
     move_choose = State()
     move_finish = State()
+
+
+class ListTask(StatesGroup):
+    show_full_task = State()
+
 
 HELP_COMMANDS = """
 <b>/start</b> - start bot.
@@ -58,45 +61,62 @@ HELP_COMMANDS = """
 <b>/help</b> - this commands.
 <b>/contact</b> - send contact bot`s developer"""
 
+
 @dp.message(Command('contact'))
 async def contact_command(message: types.Message):
     await message.answer("""
     <b>telegram</b> - @lilfinniiii
 <b>github</b> - https://github.com/lilfinniiii""", parse_mode='HTML')
 
+
 @dp.message(Command('help'))
 async def help_commands(message: types.Message):
     await message.answer(text=HELP_COMMANDS, parse_mode='HTML')
 
+
 @dp.message(Command('start'))
 async def cmd_start(message: types.Message):
-
     await message.answer("yo, i`m to-do list bot :0")
     await message.delete()
+
 
 @dp.message(Command('add'))
 async def add_task(message: types.Message, state: FSMContext):
     await message.answer('write ur task')
+    await message.answer('write "/stop" to stop')
     await state.set_state(AddTaskStates.task)
+
+
 @dp.message(AddTaskStates.task)
 async def process_task(message: types.Message, state: FSMContext):
     task = message.text
+    if message.text == '/stop':
+        await message.answer('okay, bye')
+        await state.clear()
+        return
     if task:
         await state.update_data(task=task)
         await message.answer('write the status of the task')
         await state.set_state(AddTaskStates.status)
     else:
         await message.answer("task cannot be empty. please write your task again.")
+
+
 @dp.message(AddTaskStates.status)
 async def process_status(message: types.Message, state: FSMContext):
     conn, cursor = await user_db(message.from_user.id)
     status = message.text
+    if message.text == '/stop':
+        await message.answer('okay, bye')
+        await state.clear()
+        return
     if status in valid_statuses:
         await state.update_data(status=status)
         data = await state.get_data()
         task = data.get('task')
         cursor.execute('SELECT name FROM tasks WHERE name = ?', (task,))
-        existing_task  = cursor.fetchall()
+        existing_task = cursor.fetchall()
+
         if existing_task:
             await message.answer('theres already such a task.')
             await state.clear()
@@ -107,57 +127,107 @@ async def process_status(message: types.Message, state: FSMContext):
         await state.clear()
     else:
         await message.answer(f"'status '{status}' not found'")
+
+
 @dp.message(Command('list'))
-async def show_tasks(message: types.Message):
+async def show_tasks(message: types.Message, state: FSMContext):
     conn, cursor = await user_db(message.from_user.id)
-    cursor.execute('SELECT name, status FROM tasks')
+    cursor.execute('SELECT id, name, status FROM tasks')
     tasks = cursor.fetchall()
 
     response = ""
+    total_index = 1
     for status in valid_statuses:
         response += f"<b>**{status.capitalize()}**</b>:\n"
-        for task in tasks:
-            if task[1] == status:
-                response += f"- {task[0]}\n"
+        tasks_for_status = [task for task in tasks if task[2] == status]
+
+        if tasks_for_status:
+            for task in tasks_for_status:
+                task_id, task_name, _ = task
+                response += f"{total_index}. {task_name}\n"
+                total_index += 1
 
     await message.reply(response, parse_mode='HTML')
+    await message.answer('if you want to see the full info about the task, write the number of this task.')
+    await message.answer('if you don`t want write "/stop".')
+    await state.update_data(tasks=tasks)
+    await state.set_state(ListTask.show_full_task)
+
+
+@dp.message(ListTask.show_full_task)
+async def show_info(message: types.Message, state: FSMContext):
+    conn, cursor = await user_db(message.from_user.id)
+    data = await state.get_data()
+    tasks = data.get('tasks')
+
+    if message.text == '/stop':
+        await message.answer('Okay, bye')
+        await state.clear()
+        return
+
+    try:
+        task_number = int(message.text)
+    except ValueError:
+        await message.answer("Please enter a valid task number.")
+        return
+
+    if 1 <= task_number <= len(tasks):
+        task_name = tasks[task_number - 1][1]
+        task_status = tasks[task_number - 1][2]
+
+        response = f"""
+<b>{task_name}</b>
+Status: {task_status}
+Describtion: bfasdasdaaaaadddddddddddddddd asd dsad as asd asd asd asd asd asd asd asd asd asd asd sad asd asd asd as
+Files: image"""
+        await message.answer(response, parse_mode='HTML')
+    else:
+        await message.answer("Invalid task number.")
 
 
 @dp.message(Command('delete'))
 async def delete_task(message: types.Message, state: FSMContext):
-    await message.answer('write status for delete')
-    await state.set_state((DeleteTask.waiting_for_status))
-
-@dp.message(DeleteTask.waiting_for_status)
-async def process_delete(message: types.Message, state: FSMContext):
     conn, cursor = await user_db(message.from_user.id)
-    status = message.text.lower()
-
-    if status not in valid_statuses:
-        await message.reply("invalid status. please enter one of: 'todo', 'doing', 'done'.")
-        return
-
-    cursor.execute('SELECT id, name FROM tasks WHERE status = ?', (status,))
+    cursor.execute('SELECT id, name, status FROM tasks ')
     tasks = cursor.fetchall()
 
     if not tasks:
-        await message.reply(f"no tasks in the {status} status.")
+        await message.reply(f"there is not a single task")
         return
 
-    response = f"Tasks in {status} status:\n"
-    for index, task in enumerate(tasks, start=1):
-        task_id, task_name = task
-        response += f"{index}. {task_name}\n"
+    response = ""
+    total_index = 1
+    for status in valid_statuses:
+        response += f"<b>**{status.capitalize()}**</b>:\n"
+        tasks_for_status = [task for task in tasks if task[2] == status]
+
+        if tasks_for_status:
+            for task in tasks_for_status:
+                task_id, task_name, _ = task
+                response += f"{total_index}. {task_name}\n"
+                total_index += 1
 
     response += "enter the number of the task you want to delete:"
-    await message.reply(response)
+    await message.reply(response, parse_mode="HTML")
+    await message.answer('write "/stop" to stop')
     await state.update_data(tasks=tasks)
     await state.set_state(DeleteTask.waiting_for_delete)
+
 
 @dp.message(DeleteTask.waiting_for_delete)
 async def process_task_number(message: types.Message, state: FSMContext):
     conn, cursor = await user_db(message.from_user.id)
-    task_number = int(message.text)
+    if message.text == '/stop':
+        await message.answer('okay, bye')
+        await state.clear()
+        return
+
+    try:
+        task_number = int(message.text)
+    except ValueError:
+        await message.answer("Please enter a valid task number.")
+        return
+
     data = await state.get_data()
     tasks = data.get('tasks')
 
@@ -165,47 +235,54 @@ async def process_task_number(message: types.Message, state: FSMContext):
         task_id_to_delete = tasks[task_number - 1][0]
         cursor.execute('DELETE FROM tasks WHERE id = ?', (task_id_to_delete,))
         conn.commit()
-        await message.reply(f"task - {tasks[task_number - 1][0]} deleted successfully!")
+        await message.reply(f"task - '{tasks[task_number - 1][1]}' deleted successfully!")
         await state.clear()
     else:
         await message.reply("invalid task number.")
 
 
-
 @dp.message(Command('move'))
 async def move_task(message: types.Message, state: FSMContext):
-    await message.answer('write status for move')
-    await state.set_state((MoveTask.move_status))
-
-@dp.message(MoveTask.move_status)
-async def move_status(message: types.Message, state: FSMContext):
     conn, cursor = await user_db(message.from_user.id)
-    status = message.text.lower()
-
-    if status not in valid_statuses:
-        await message.reply("invalid status. please enter one of: 'todo', 'doing', 'done'.")
-        return
-
-    cursor.execute('SELECT id, name FROM tasks WHERE status = ?', (status,))
+    cursor.execute('SELECT id, name, status FROM tasks ')
     tasks = cursor.fetchall()
 
     if not tasks:
-        await message.reply(f"No tasks in the {status} status.")
+        await message.reply(f"there is not a single task")
         return
 
-    response = f"tasks in {status} status:\n"
-    for index, task in enumerate(tasks, start=1):
-        task_id, task_name = task
-        response += f"{index}. {task_name}\n"
+    response = ""
+    total_index = 1
+    for status in valid_statuses:
+        response += f"<b>**{status.capitalize()}**</b>:\n"
+        tasks_for_status = [task for task in tasks if task[2] == status]
+
+        if tasks_for_status:
+            for task in tasks_for_status:
+                task_id, task_name, _ = task
+                response += f"{total_index}. {task_name}\n"
+                total_index += 1
 
     response += "enter the number of the task you want to move:"
     await message.reply(response)
+    await message.answer('write "/stop" to stop')
     await state.update_data(tasks=tasks)
     await state.set_state(MoveTask.move_choose)
 
+
 @dp.message(MoveTask.move_choose)
 async def move_choose(message: types.Message, state: FSMContext):
-    task_number = int(message.text)
+    if message.text == '/stop':
+        await message.answer('okay, bye')
+        await state.clear()
+        return
+
+    try:
+        task_number = int(message.text)
+    except ValueError:
+        await message.answer("Please enter a valid task number.")
+        return
+
     data = await state.get_data()
     tasks = data.get('tasks')
     if 1 <= task_number <= len(tasks):
@@ -216,10 +293,15 @@ async def move_choose(message: types.Message, state: FSMContext):
     else:
         await message.reply("invalid task number.")
 
+
 @dp.message(MoveTask.move_finish)
 async def move_finish(message: types.Message, state: FSMContext):
     conn, cursor = await user_db(message.from_user.id)
     new_status = message.text.lower()
+    if message.text == '/stop':
+        await message.answer('okay, bye')
+        await state.clear()
+        return
     data = await state.get_data()
     tasks = data.get('tasks')
     task_number = data.get('task_number')
@@ -236,17 +318,9 @@ async def move_finish(message: types.Message, state: FSMContext):
         await message.reply("invalid new status.")
 
 
-
-
-
-
-
-
-
-
-
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
